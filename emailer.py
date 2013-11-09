93de0b5fb69351efa4fe81734a8eb01f566b51c1
varunrau <varunrau@gmail.com>
2013-11-09 09:12:59 -0800

    removed unnecessary password input by creating new email

--- emailer.py
+++ emailer.py
@@ -1,15 +1,18 @@
 import smtplib
 
 class Emailer:
-	def __init__(self, email_address, password):
+	def __init__(self, email_address):
+		self.FROM_EMAIL = "websitepolling@gmail.com"
+		self.PASSWORD = "ipollyoursites"
 		self.email_address = email_address
-		self.password = password
 		self.init_server()
 
 	def init_server(self):
 		self.server = smtplib.SMTP("smtp.gmail.com", 587)
-		self.server.login(self.email_address, self.password)
+		self.server.starttls()
+		self.server.login(self.FROM_EMAIL, self.PASSWORD)
 
-	def send_message(message="\nYou've got mail!"):
-		self.server.sendmail(self.email_address, self.email_address, message)
+	def send_message(self, message="\nYou've got mail!"):
+		email = "Subject: %s\n\n%s" % ("Your reminder from Website Polling", message)
+		self.server.sendmail(self.FROM_EMAIL, self.email_address, email)
 
--- poll.py
+++ poll.py
@@ -9,12 +9,11 @@ interval = 3600
 
 class Poller:
 
-	def __init__(self, page, interval, email, password, message):
+	def __init__(self, page, interval, email, message):
 		self.interval = interval
 		self.url = page
 		self.cached_page = self.poll()
 		self.email = email
-		self.password = password
 		self.message = message
 		self.interval = interval
 
@@ -27,7 +26,7 @@ class Poller:
 	@sched.interval_schedule(minutes=interval)
 	def check_page(self):
 		if self.did_change():
-			emailer = Emailer(self.email, self.password)
+			emailer = Emailer(self.email)
 			emailer.send_message(self.message)
 			sched.shutdown()
 
@@ -38,11 +37,10 @@ if __name__ == "__main__":
 	parser = ArgumentParser(description="Emails you when a given website changes")
 	parser.add_argument("-u", "--url", nargs="?", type=str, help="The website you would like to check")
 	parser.add_argument("-e", "--email", nargs="?", type=str, help="your email")
-	parser.add_argument("-p", "--password", nargs="?", type=str, help="your email password")
 	parser.add_argument("-i", "--interval", type=int, help="How often you would like to check (in minutes)")
 	parser.add_argument("-m", "--message", type=str, help="The message you would like to see in your email notification")
 	args = parser.parse_args()
 	sched.start()
 	interval = args.interval
-	poller = Poller(args.url, args.interval, args.email, args.password, args.message)
+	poller = Poller(args.url, args.interval, args.email, args.message)
 
